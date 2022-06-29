044889027fae760431e311a4aa5a7b8f1c249db2
Shaps Benkau <shapsuk@me.com>
2022-06-29 15:02:46 +0100

    ShareLink isn't ready, so comments out the demo for that

--- Shared/Backport Demos/ShareLinkDemo.swift
+++ Shared/Backport Demos/ShareLinkDemo.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import SwiftUIBackports
 
 struct ShareLinkDemo: View {
     var body: some View {
@@ -12,9 +13,79 @@ struct ShareLinkDemo: View {
 }
 
 private struct Demo: View {
+    let url = URL(string: "https://picsum.photos/200/200")!
+    @Backport.ScaledMetric private var padding: CGFloat = 5
+
+    @MainActor
     var body: some View {
-        List {
-//            Backport.ShareLink
+        if #available(iOS 15, *) {
+            List {
+//                Backport.Section("Backport") {
+//                    Backport.ShareLink("URL with label", item: url)
+//                    Backport.ShareLink("Text with label", item: "SwiftUI Backports")
+//                    Backport.ShareLink(item: url)
+//                    Backport.ShareLink(items: [url]) { item in
+//                        Backport.SharePreview("Preview", icon: Image(systemName: "circle"))
+//                    }
+//                }
+//
+//                if #available(iOS 16, *) {
+//                    Backport.Section("Native") {
+//                        ShareLink("URL with label", item: url)
+//                        ShareLink("Text with label", item: "SwiftUI Backports")
+//                        ShareLink(item: url)
+//                        ShareLink(items: ["Share Text"]) { item in
+//                            SharePreview("Preview", icon: Image(systemName: "safari"))
+//                        }
+//                    }
+//                }
+            }
         }
     }
 }
+//
+//struct BackportTest {
+//    struct ImageDocumentLayer: BackportTransferable {
+//        init(data: Data) throws { }
+//
+//        func data() -> Data { .init() }
+//        func pngData() -> Data { .init() }
+//
+//        static var transferRepresentation: some BackportTransferRepresentation {
+//            Backport.DataRepresentation(contentType: "") { layer in
+//                layer.data()
+//            } importing: { data in
+//                try ImageDocumentLayer(data: data)
+//            }
+//
+//            Backport.DataRepresentation(exportedContentType: "") { layer in
+//                layer.pngData()
+//            }
+//        }
+//    }
+//}
+//
+//@available(iOS 16, *)
+//struct NativeTest {
+//    struct ImageDocumentLayer: Transferable {
+//        init(data: Data) throws { }
+//
+//        func data() -> Data { .init() }
+//        func pngData() -> Data { .init() }
+//
+//        static var transferRepresentation: some TransferRepresentation {
+//            DataRepresentation(exportedContentType: "") { layer in
+//                layer
+//            }
+//            DataRepresentation(contentType: .image) { layer in
+//                layer.data()
+//            } importing: { data in
+//                try ImageDocumentLayer(data: data)
+//            }
+//
+//            DataRepresentation(exportedContentType: .png) { layer in
+//                layer.pngData()
+//            }
+//        }
+//    }
+//}
