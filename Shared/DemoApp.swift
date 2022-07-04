219ebbf1ded81339ccb0f6f68359a8f312f2b969
Shaps Benkau <shapsuk@me.com>
2022-07-04 09:17:41 +0100

    Removes ShareLink DEmo

--- Shared/DemoApp.swift
+++ Shared/DemoApp.swift
@@ -13,7 +13,6 @@ struct RootView: View {
                     ProgressDemo()
                     RefreshableDemo()
                     StateObjectDemo()
-                    ShareLinkDemo()
                 }
 #if os(iOS)
                 if #available(iOS 14, *) {
