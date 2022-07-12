3cb6c0ac44ea8f7103ddde6ebc2b0b8d8d04bd1f
Shaps Benkau <shapsuk@me.com>
2022-07-12 18:38:36 +0100

    Adds availability, docs and demo

--- Shared/Backport Demos/LabeledContentDemo.swift
+++ Shared/Backport Demos/LabeledContentDemo.swift
@@ -13,17 +13,61 @@ struct LabeledContentDemo: View {
 }
 
 private struct Demo: View {
+    var people: [Person] = [
+        .init(name: "Shaps", age: 42, pets: [
+            .init(name: "Scruffy", species: "Dog"),
+            .init(name: "Lippy", species: "Cat")
+        ]),
+        .init(name: "Anton", age: 5, pets: [
+            .init(name: "Polly", species: "Alpaca"),
+            .init(name: "Mr Bear", species: "Teddy")
+        ])
+    ]
+
     var body: some View {
-        List {
+        Form {
             Backport.Section("Backport") {
+                ForEach(people) { person in
+                    Backport.LabeledContent("Name", value: person.name)
 
-            }
-
-            if #available(iOS 16, macOS 13, *) {
-                Backport.Section("Native") {
-
+                    if !person.pets.isEmpty {
+                        ForEach(person.pets) { pet in
+                            Backport.LabeledContent(pet.species, value: pet.name)
+                        }
+                    }
                 }
             }
+//            .labelsHidden()
+
+            if #available(iOS 16, macOS 13, *) {
+                Section("Native") {
+                    ForEach(people) { person in
+                        LabeledContent("Name", value: person.name)
+
+                        if !person.pets.isEmpty {
+                            ForEach(person.pets) { pet in
+                                LabeledContent(pet.species, value: pet.name)
+                            }
+                        }
+                    }
+                }
+//                .labelsHidden()
+            }
         }
+        .backport.navigationTitle("People")
+    }
+}
+
+private struct Person: Identifiable {
+    var id: String { name }
+
+    var name: String
+    var age: Int
+    var pets: [Pet]
+
+    struct Pet: Identifiable {
+        var id: String { name }
+        var name: String
+        var species: String
     }
 }
