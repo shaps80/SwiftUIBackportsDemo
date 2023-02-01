ca009c65ee70d1d69d5da5db406cb7dc8bda77b5
Shaps Benkau <shapsuk@me.com>
2023-02-01 11:30:44 +0000

    Updates Demo for PhotosPicker

--- iOS/Backport Demos/PhotosPickerDemo.swift
+++ iOS/Backport Demos/PhotosPickerDemo.swift
@@ -14,24 +14,35 @@ struct PhotosPickerDemo: View {
 }
 
 struct PhotosDemoView: View {
+    @State private var showPicker: Bool = false
     @State private var selection: [Backport<Any>.PhotosPickerItem] = []
-    @State private var text: String = ""
     
     var body: some View {
         List {
-            Backport.Section("iOS 13+") {
-                Backport.PhotosPicker(
-                    selection: $selection,
-                    matching: nil
-                ) {
-                    Backport.Label("Choose Photo", systemImage: "photo")
+            Backport.Section("Backport") {
+                if #available(iOS 15, *) {
+                    Backport.PhotosPicker(selection: $selection, maxSelectionCount: 3, selectionBehavior: .ordered, matching: .all(of: [.slomoVideos, .videos])) {
+                        Label("Choose Photos", systemImage: "photo")
+                    }
+                } else if #available(iOS 14, *) {
+                    Button {
+                        showPicker = true
+                    } label: {
+                        Text("Show iOS 14 Picker")
+                    }
+                    .backport.photosPicker(isPresented: $showPicker, selection: $selection, maxSelectionCount: 3, matching: .any(of: [.images, .videos]))
+                } else {
+                    Button {
+                        showPicker = true
+                    } label: {
+                        Text("Show iOS 13 Picker")
+                    }
+                    .backport.photosPicker(isPresented: $showPicker, selection: $selection)
                 }
             }
 
-            TextField("Enter text", text: $text)
-
             if #available(iOS 16, *) {
-                Backport.Section("iOS 16+") {
+                Backport.Section("Native") {
                     PhotosPicker(
                         selection: .constant([]),
                         matching: nil
