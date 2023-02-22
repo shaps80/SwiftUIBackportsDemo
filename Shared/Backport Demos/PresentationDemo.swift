1a323b2aaf7f9340aa8ad80a8a698a50cd7f4310
Shaps Benkau <shapsuk@me.com>
2023-02-22 01:31:56 +0000

    Updates Demo project

--- Shared/Backport Demos/PresentationDemo.swift
+++ Shared/Backport Demos/PresentationDemo.swift
@@ -5,15 +5,62 @@ import SwiftUIBackports
 @available(iOS 14, *)
 struct PresentationDemo: View {
     @State private var showSheet: Bool = false
+    @State private var backgroundInteraction: Backport<Any>.PresentationBackgroundInteraction = .disabled
+    @State private var contentInteraction: Backport<Any>.PresentationContentInteraction = .resizes
+    @State private var detents: Set<Backport.PresentationDetent> = [.medium, .large]
+    @State private var selection: Backport.PresentationDetent = .medium
+    @State private var customRadius: Bool = false
+    @State private var cornerRadius: CGFloat = 20
 
     var body: some View {
-        Button {
-            showSheet = true
+        NavigationLink {
+            List {
+                Section {
+                    Picker("Background Interaction", selection: $backgroundInteraction) {
+                        Text("Disabled")
+                            .tag(Backport<Any>.PresentationBackgroundInteraction.disabled)
+
+                        Text("Enabled")
+                            .tag(Backport<Any>.PresentationBackgroundInteraction.enabled)
+
+                        Text("Up to Medium")
+                            .tag(Backport<Any>.PresentationBackgroundInteraction.enabled(upThrough: .medium))
+
+                        Text("Up to Large")
+                            .tag(Backport<Any>.PresentationBackgroundInteraction.enabled(upThrough: .large))
+                    }
+                    .pickerStyle(.menu)
+
+                    Picker("Content Interaction", selection: $contentInteraction) {
+                        Text("Resizes")
+                            .tag(Backport<Any>.PresentationContentInteraction.resizes)
+
+                        Text("Scrolls")
+                            .tag(Backport<Any>.PresentationContentInteraction.scrolls)
+                    }
+                    .pickerStyle(.menu)
+                }
+
+                Section {
+                    Button {
+                        showSheet = true
+                    } label: {
+                        Text("Show Sheet")
+                    }
+                }
+                .popover(isPresented: $showSheet) {
+                    DemoView(detents: $detents, selection: $selection, customRadius: $customRadius, cornerRadius: $cornerRadius)
+                        .presentationDetentsIfAvailable(
+                            detents: detents,
+                            selection: $selection,
+                            backgroundInteraction: backgroundInteraction,
+                            contentInteraction: contentInteraction,
+                            cornerRadius: customRadius ? cornerRadius : nil
+                        )
+                }
+            }
         } label: {
             Text("Presentation")
-        }
-        .sheet(isPresented: $showSheet) {
-            DemoView()
         }
     }
 }
@@ -22,17 +69,27 @@ struct PresentationDemo: View {
 private struct DemoView: View {
     @Environment(\.backportDismiss) private var dismiss
 
-    @State private var selection: Backport.PresentationDetent = .medium
     @State private var visible: Backport.Visibility = .hidden
-    @State private var detents: Set<Backport.PresentationDetent> = [.medium, .large]
     @State private var isModal: Bool = false
     @State private var enablePrompt: Bool = true
     @State private var showPrompt: Bool = false
-    @State private var undimmedDetent: Backport<Any>.PresentationDetent?
+
+    @Binding var detents: Set<Backport<Any>.PresentationDetent>
+    @Binding var selection: Backport<Any>.PresentationDetent
+    @Binding var customRadius: Bool
+    @Binding var cornerRadius: CGFloat
 
     var body: some View {
         NavigationView {
             List {
+                Section {
+                    Toggle("Custom Radius", isOn: $customRadius.animation())
+
+                    if customRadius {
+                        Slider(value: $cornerRadius, in: 0...100)
+                    }
+                }
+
                 Section {
                     Button {
                         visible = visible == .hidden ? .visible : .hidden
@@ -46,13 +103,6 @@ private struct DemoView: View {
                         Text(selection == .medium ? "Expand" : "Collapse")
                     }
                     .disabled(detents.count == 1)
-
-                    Button {
-                        undimmedDetent = undimmedDetent == .medium ? nil : .medium
-                    } label: {
-                        Text("Passthrough")
-                            .checkmark(undimmedDetent != nil ? .visible : .hidden)
-                    }
                 }
 
                 Section {
@@ -95,6 +145,14 @@ private struct DemoView: View {
                             Text("Prompt on Dismiss")
                                 .checkmark(enablePrompt ? .visible : .hidden)
                         }
+                        .actionSheet(isPresented: $showPrompt) {
+                            ActionSheet(title: Text("Dismiss"), buttons: [
+                                .default(Text("OK")) {
+                                    dismiss()
+                                },
+                                .cancel()
+                            ])
+                        }
                     }
                 }
             }
@@ -112,26 +170,26 @@ private struct DemoView: View {
             }
         }
         .backport.presentationDragIndicator(visible)
-        .presentationDetentsIfAvailable(detents, selection: $selection, largestUndimmedDetent: undimmedDetent)
         .backport.interactiveDismissDisabled(isModal) {
             if enablePrompt { showPrompt = true }
         }
-        .actionSheet(isPresented: $showPrompt, content: {
-            ActionSheet(title: Text("Dismiss"), buttons: [
-                .default(Text("OK")) {
-                    dismiss()
-                },
-                .cancel()
-            ])
-        })
     }
 }
 
 extension View {
     @ViewBuilder
-    func presentationDetentsIfAvailable(_ detents: Set<Backport<Any>.PresentationDetent>, selection: Binding<Backport<Any>.PresentationDetent>, largestUndimmedDetent: Backport<Any>.PresentationDetent?) -> some View {
+    func presentationDetentsIfAvailable(
+        detents: Set<Backport<Any>.PresentationDetent>,
+        selection: Binding<Backport<Any>.PresentationDetent>,
+        backgroundInteraction: Backport<Any>.PresentationBackgroundInteraction,
+        contentInteraction: Backport<Any>.PresentationContentInteraction,
+        cornerRadius: CGFloat?
+    ) -> some View {
         if #available(iOS 15, *) {
-            backport.presentationDetents(detents, selection: selection, largestUndimmedDetent: largestUndimmedDetent)
+            backport.presentationDetents(detents, selection: selection)
+                .backport.presentationBackgroundInteraction(backgroundInteraction)
+                .backport.presentationContentInteraction(contentInteraction)
+                .backport.presentationCornerRadius(cornerRadius)
         } else {
             self
         }
