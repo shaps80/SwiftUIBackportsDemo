3f92935b1163cedc2eb08bcea1b78e3c30832e7c
Shaps Benkau <shapsuk@me.com>
2023-06-16 14:27:11 +0100

    Pulls PhotosPicker as its incomplete!

--- iOS/Backport Demos/PhotosPickerDemo.swift
+++ iOS/Backport Demos/PhotosPickerDemo.swift
@@ -19,38 +19,39 @@ struct PhotosDemoView: View {
     
     var body: some View {
         List {
-            Backport.Section("Backport") {
-                if #available(iOS 15, *) {
-                    Backport.PhotosPicker(selection: $selection, maxSelectionCount: 3, selectionBehavior: .ordered, matching: .any(of: [.slomoVideos, .timelapseVideos])) {
-                        Label("Choose Photos", systemImage: "photo")
-                    }
-                } else if #available(iOS 14, *) {
-                    Button {
-                        showPicker = true
-                    } label: {
-                        Text("Show iOS 14 Picker")
-                    }
-                    .backport.photosPicker(isPresented: $showPicker, selection: $selection, maxSelectionCount: 3, matching: .any(of: [.videos]))
-                } else {
-                    Button {
-                        showPicker = true
-                    } label: {
-                        Text("Show iOS 13 Picker")
-                    }
-                    .backport.photosPicker(isPresented: $showPicker, selection: $selection, matching: .livePhotos)
-                }
-            }
-
-            if #available(iOS 16, *) {
-                Backport.Section("Native") {
-                    PhotosPicker(
-                        selection: .constant([]),
-                        matching: nil
-                    ) {
-                        Label("Choose Photos", systemImage: "photo")
-                    }
-                }
-            }
+            Text("TBD")
+//            Backport.Section("Backport") {
+//                if #available(iOS 15, *) {
+//                    Backport.PhotosPicker(selection: $selection, maxSelectionCount: 3, selectionBehavior: .ordered, matching: .any(of: [.slomoVideos, .timelapseVideos])) {
+//                        Label("Choose Photos", systemImage: "photo")
+//                    }
+//                } else if #available(iOS 14, *) {
+//                    Button {
+//                        showPicker = true
+//                    } label: {
+//                        Text("Show iOS 14 Picker")
+//                    }
+//                    .backport.photosPicker(isPresented: $showPicker, selection: $selection, maxSelectionCount: 3, matching: .any(of: [.videos]))
+//                } else {
+//                    Button {
+//                        showPicker = true
+//                    } label: {
+//                        Text("Show iOS 13 Picker")
+//                    }
+//                    .backport.photosPicker(isPresented: $showPicker, selection: $selection, matching: .livePhotos)
+//                }
+//            }
+//
+//            if #available(iOS 16, *) {
+//                Backport.Section("Native") {
+//                    PhotosPicker(
+//                        selection: .constant([]),
+//                        matching: nil
+//                    ) {
+//                        Label("Choose Photos", systemImage: "photo")
+//                    }
+//                }
+//            }
         }
         .backport.navigationTitle("Photos Picker")
     }
