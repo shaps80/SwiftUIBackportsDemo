6e015650282c9a7580a47b265a6568b61d701efd
Shaps Benkau <shapsuk@me.com>
2023-08-31 21:17:32 +0100

    Updated as per new API updates Updated layout and presentations for inline and popover to match latest updates

--- Shared/Extras Demos/Backport Demos/TipsDemo.swift
+++ Shared/Extras Demos/Backport Demos/TipsDemo.swift
@@ -20,28 +20,111 @@ struct TipsDemo: View {
 @available(iOS 13, macOS 11, *)
 private struct Demo: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            if #available(iOS 15, macOS 12, *) {
-                Backport.TipView(Tip1())
-                    .backport.tipBackground(.tint.opacity(0.1))
-                    .frame(maxWidth: 320)
-                    .backport.popoverTip(Tip1())
-            } else {
-                Backport.TipView(Tip1())
-                    .backport.tipBackground(color: .accentColor.opacity(0.1))
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                if #available(iOS 17, macOS 14, *) {
+//                    TipView(Tip1())
+//                    TipView(Tip2())
+//                        .popoverTip(PopoverTip())
+                }
+
+
+
+                if #available(iOS 15, macOS 12, *) {
+                    Backport.TipView(BackportTip1())
+                        .backport.popoverTip(BackportTip1())
+                } else {
+                    Backport.TipView(BackportTip1())
+                }
             }
-        }
-        .padding()
-        .backport.task {
-            try? await Backport.Tips.configure {
-                DisplayFrequency(.daily)
-                DatastoreLocation(.applicationDefault, shouldReset: true)
+            .padding()
+            .backport.task {
+                do {
+                    try Backport.Tips.resetDatastore()
+                    try Backport.Tips.configure([
+                        .displayFrequency(.immediate),
+                        .datastoreLocation(.applicationDefault),
+                    ])
+
+                    if #available(iOS 17, macOS 14, *) {
+                        try Tips.resetDatastore()
+                        try Tips.configure([
+                            .displayFrequency(.immediate),
+                            .datastoreLocation(.applicationDefault),
+                        ])
+                    }
+                } catch {
+                    print(error)
+                }
             }
         }
     }
 }
 
-struct Tip1: BackportTip {
+#if canImport(TipKit)
+import TipKit
+#endif
+@available(iOS 17, macOS 14, *)
+struct PopoverTip: TipKit.Tip {
+    var id: String { "get.started" }
+    var title: Text { .init("Welcome") }
+
+    var message: Text? {
+        Text("Foobar?")
+    }
+
+    var actions: [Action] {
+        Action(title: "Foo 1")
+        Action(title: "Bar 2")
+    }
+
+    var options: [Option] {
+        MaxDisplayCount(.max)
+        IgnoresDisplayFrequency(true)
+    }
+}
+
+@available(iOS 17, macOS 14, *)
+struct Tip1: TipKit.Tip {
+    var id: String { "get.started.1" }
+    var title: Text { .init("Welcome") }
+
+    var message: Text? {
+        Text("Foobar?")
+    }
+
+    var actions: [Action] {
+        Action(title: "Foo 1")
+        Action(title: "Bar 2")
+    }
+
+    var options: [Option] {
+        MaxDisplayCount(.max)
+        IgnoresDisplayFrequency(true)
+    }
+}
+
+@available(iOS 17, macOS 14, *)
+struct Tip2: TipKit.Tip {
+    var id: String { "get.started.2" }
+    var title: Text { .init("Welcome") }
+
+    var message: Text? {
+        Text("Foobar?")
+    }
+
+    var actions: [Action] {
+        Action(title: "Foo 1")
+        Action(title: "Bar 2")
+    }
+
+    var options: [Option] {
+        MaxDisplayCount(.max)
+        IgnoresDisplayFrequency(true)
+    }
+}
+
+struct BackportTip1: BackportTip {
     var id: String { "get.started" }
     var title: Text { .init("Welcome") }
 
