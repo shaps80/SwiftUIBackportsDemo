ec98cd58e2adb84d7267cd60a5d8f2ae6c1016d6
Shaps Benkau <shapsuk@me.com>
2026-06-11 20:02:31 +0100

    More cleanup

--- Shared/DemoApp.swift
+++ Shared/DemoApp.swift
@@ -21,15 +21,9 @@ struct RootView: View {
                 Demo(TextEditorDemo())
                 Demo(SensoryFeedbackDemo())
 
-                if #available(iOS 14, *) {
-                    Demo(HostingCollectionDemo())
-                    Demo(HostingTableDemo())
-                }
-
-                if #available(iOS 15, *) {
-                    Demo(PresentationDemo())
-
-                }
+                Demo(HostingCollectionDemo())
+                Demo(HostingTableDemo())
+                Demo(PresentationDemo())
 #endif
             }
             .inlineTitle("Demos")
@@ -40,46 +34,10 @@ struct RootView: View {
     }
 }
 
-#if os(iOS)
-@main
-final class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool { true }
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-}
-
-final class SceneDelegate: UIResponder, UISceneDelegate {
-    var window: UIWindow?
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let scene = scene as? UIWindowScene else { return }
-        window = UIWindow(windowScene: scene)
-        window?.rootViewController = UIHostingController(
-            rootView: RootView()
-        )
-        window?.makeKeyAndVisible()
-    }
-}
-#else
-@available(macOS 11, *)
 @main struct DemoApp: App {
     var body: some Scene {
         WindowGroup {
             RootView()
-                .hiddenToolbarBackground()
         }
     }
 }
-
-extension View {
-    @ViewBuilder
-    func hiddenToolbarBackground() -> some View {
-        if #available(iOS 18, tvOS 18, macOS 15, watchOS 11, *) {
-            toolbarBackgroundVisibility(.hidden, for: .windowToolbar)
-        } else {
-            self
-        }
-    }
-}
-#endif
